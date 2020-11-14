7398ac31d57e46783f791047942445de8f54c245
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2020-11-14 14:22:24 +0000

    abstracted db components

--- shopping.py
+++ shopping.py
@@ -1,10 +1,29 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, g, render_template
 
 app = Flask(__name__)
 
+# load the configuration to smooth database connections
+app.config.from_object(__name__)
+
 # database details - to remove some duplication
 db_name = 'shopping_data.db'
+
+# code based on example at https://github.com/mjhea0/flaskr-tdd
+
+def connect_db():
+    conn = sqlite3.connect(db_name, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 @app.route('/')
 def index():
@@ -17,53 +36,41 @@ def index():
 
 @app.route('/customers')
 def customers():
-    conn = sqlite3.connect(db_name)
-    conn.row_factory = sqlite3.Row
-    cur = conn.cursor()
+    db = get_db()
     # get results from customers
-    cur.execute("select * from customers")
+    cur = db.execute("select * from customers")
     rows = cur.fetchall()
-    conn.close()
     return render_template('customers.html', rows=rows)
 
 @app.route('/customer_details/<id>')
 def customer_details(id):
-    conn = sqlite3.connect(db_name)
-    conn.row_factory = sqlite3.Row
-    cur = conn.cursor()
+    db = get_db()
     # get results from customers
-    cur.execute("select * from customers WHERE id=?", (id))
+    cur = db.execute("select * from customers WHERE id=?", (id))
     customer = cur.fetchall()
-    cur.execute("select * from orders WHERE customer_id=?", (id))
+    cur = db.execute("select * from orders WHERE customer_id=?", (id))
     # we call this 'rows' instead of 'orders' as the later causes confusion in 
     # call to order_details page, which has object called 'orders' too
     rows = cur.fetchall()
-    conn.close()
     return render_template('customer_details.html', customer=customer, rows=rows)
 
 @app.route('/orders')
 def orders():
-    conn = sqlite3.connect(db_name)
-    conn.row_factory = sqlite3.Row
-    cur = conn.cursor()
+    db = get_db()
     # get results from orders
-    cur.execute("select * from orders")
+    cur = db.execute("select * from orders")
     rows = cur.fetchall()
-    conn.close()
     return render_template('orders.html', rows=rows)
 
 @app.route('/order_details/<id>')
 def order_details(id):
-    conn = sqlite3.connect(db_name)
-    conn.row_factory = sqlite3.Row
-    cur = conn.cursor()
+    db = get_db()
     # get results from orders
-    cur.execute("select * from orders WHERE id=?", (id))
+    cur = db.execute("select * from orders WHERE id=?", (id))
     order = cur.fetchall()
     customer_id = order[0]['customer_id']
-    cur.execute("select * from customers WHERE id=?", (customer_id,))
+    cur = db.execute("select * from customers WHERE id=?", (customer_id,))
     customer = cur.fetchall()
-    cur.execute("select * from line_items WHERE order_id=?", (id))
+    cur = db.execute("select * from line_items WHERE order_id=?", (id))
     items = cur.fetchall()
-    conn.close()
     return render_template('order_details.html', order=order, items=items, customer=customer)
